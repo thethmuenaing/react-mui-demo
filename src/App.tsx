4a436091e4416e124d95d87fe24e3b0eb62bdbad
thethmuenaing <thethmuenaing22112002@gmail.com>
2022-10-18 13:33:12 +0630

    build MuiSelect component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import './App.css';
 // import { MuiTypography } from './components/MuiTypography';
 // import { MuiButton } from './components/MuiButton';
-import { MuiTextField } from './components/MuiTextField';
+// import { MuiTextField } from './components/MuiTextField';
+import { MuiSelect } from './components/MuiSelect';
 
 function App() {
   return (
     <div className="App">
       {/* <MuiTypography/> */}
       {/* <MuiButton/> */}
-      <MuiTextField/>
+      {/* <MuiTextField/> */}
+      <MuiSelect/>
     </div>
   );
 }
