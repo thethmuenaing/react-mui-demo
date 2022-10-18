e597bafbbd2477528c8ded744f3e0bea4360b264
thethmuenaing <thethmuenaing22112002@gmail.com>
2022-10-18 14:16:19 +0630

    build MuiRadioButton component

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import './App.css';
 // import { MuiTypography } from './components/MuiTypography';
 // import { MuiButton } from './components/MuiButton';
 // import { MuiTextField } from './components/MuiTextField';
-import { MuiSelect } from './components/MuiSelect';
+// import { MuiSelect } from './components/MuiSelect';
+import { MuiRadioButton } from './components/MuiRadioButton';
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
       {/* <MuiTypography/> */}
       {/* <MuiButton/> */}
       {/* <MuiTextField/> */}
-      <MuiSelect/>
+      {/* <MuiSelect/> */}
+      <MuiRadioButton/>
     </div>
   );
 }
