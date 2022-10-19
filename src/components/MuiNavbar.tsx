44798eeee2f18d97bab988f02d424d65983b3f29
thethmuenaing <thethmuenaing22112002@gmail.com>
2022-10-19 20:50:03 +0630

    build MuiNavbar component

--- src/components/MuiNavbar.tsx
+++ src/components/MuiNavbar.tsx
@@ -4,12 +4,24 @@ import {
 	IconButton, 
 	Typography, 
 	Stack, 
-	Button 
+	Button,
+	Menu,
+	MenuItem 
 } from "@mui/material"
 import CatchingPokemonIcon from "@mui/icons-material/CatchingPokemon"
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown"
+import React, { useState } from "react"
 
 
 export const MuiNavbar = () => {
+	const [anchorEl , setAnchorEl ] = useState<null | HTMLElement>(null)
+	const open = Boolean(anchorEl)
+	const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+		setAnchorEl(event.currentTarget)
+	}
+	const handleClose = () => {
+		setAnchorEl(null)
+	}
 	return (
 		<AppBar position="static">
 			<Toolbar>
@@ -23,8 +35,39 @@ export const MuiNavbar = () => {
 					<Button color="inherit">Features</Button>
 					<Button color="inherit">Pricing</Button>
 					<Button color="inherit">About</Button>
+					<Button 
+						color="inherit" 
+						id="resources-button" 
+						onClick={handleClick}
+						aria-controls={open ? "resources-menu" : undefined}
+						aria-haspopup="true"
+						aria-expanded={open ? "true" : undefined}
+						endIcon={<KeyboardArrowDownIcon/>}
+					>
+						Resources
+					</Button>
 					<Button color="inherit">Login</Button>
 				</Stack>
+				<Menu 
+					id="resources-menu" 
+					anchorEl={anchorEl} 
+					open={open}
+					MenuListProps={{
+						"aria-labelledby" : "resources-button"
+					}}
+					onClose={handleClose}
+					anchorOrigin={{
+						vertical: "bottom",
+						horizontal: "right",
+					}}
+					transformOrigin={{
+						vertical: "top",
+						horizontal: "right",
+					}}
+				>
+					<MenuItem onClick={handleClose}>Blog</MenuItem>
+					<MenuItem onClick={handleClose}>Podcast</MenuItem>
+				</Menu>
 			</Toolbar>
 		</AppBar>
 	)
